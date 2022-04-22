da9985e3a0789c80832c9da1040c17e9a36cc0aa
bjornhofer <95904463+bjornhofer@users.noreply.github.com>
2022-04-22 15:00:37 +0200

    added firewall

--- modules/vwan_hub/main.tf
+++ modules/vwan_hub/main.tf
@@ -9,3 +9,11 @@ resource "azurerm_virtual_hub" "vwan_hub" {
   resource_group_name = data.azurerm_resource_group.rg.name
   virtual_wan_id      = var.vwan_id
 }
+
+resource "azurerm_firewall" "test" {
+  name                = "vwan-hub-fw-${lower(replace(var.vwan_hub_region, " ", ""))}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = var.vwan_hub_region
+  threat_intel_mode   = ""
+  sku_name            = "AZFW_Hub"
+}
